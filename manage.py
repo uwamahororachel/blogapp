f67f115383958639118c72aa1cbe159be0890813
uwamahororachel <racheluwamahoro45@gmail.com>
2021-03-30 23:08:20 +0200

    manage file config

---
+++ manage.py
@@ -0,0 +1,24 @@
+from flask_script import Manager,Server
+from app import create_app,db
+from app.models import User,Role,Blog
+from  flask_migrate import Migrate, MigrateCommand
+
+
+
+# Creating app instance
+# app = create_app('production')
+app = create_app('development')
+# app = create_app('test')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+@manager.shell
