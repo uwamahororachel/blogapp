30ebab8116fa3a3e13117fe89438e05e1a20910f
uwamahororachel <racheluwamahoro45@gmail.com>
2021-03-30 23:10:32 +0200

    config

--- config.py
+++ config.py
@@ -41,3 +41,8 @@ class DevConfig(Config):
 # class DevConfig(Config):
 #     DEBUG = True
 
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='ff73536b7f0e63d45fe4f61ecf8eb703'
+export SECRET_KEY ='arandomstring'
+export MAIL_USERNAME='racheluwamahoro55@gmail.com'
+export MAIL_PASSWORD='takecare1'
+python3.6 manage.py server
+
